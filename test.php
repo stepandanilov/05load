dac8a2897be0f53c1b6b2c6f79d72f16dff7fed5
Aleksandr Pavlov <7911562+av-pavlov@users.noreply.github.com>
2020-11-30 02:08:31 +0000

    reformat php

--- test.php
+++ test.php
@@ -3,8 +3,13 @@
 <meta charset="utf-8">
 <body>
 <h1>!Вашафамилия!</h1>
-<?php $iter = 200000; $salt = openssl_random_pseudo_bytes(16); $hash = hash_pbkdf2("sha256", "password", $salt, $iter); echo $hash . "<b> reported by </b>" . gethostname(); ?>
-<h1>Tag Value</h1>
+<?php
+$iter = 200000;
+$salt = openssl_random_pseudo_bytes(16);
+$hash = hash_pbkdf2("sha256", "password", $salt, $iter);
+echo $hash . "<b> reported by </b>" . gethostname(); 
+?>
+<h1>Значение тега name</h1>
 <?php
 require('vendor/autoload.php');
 $sdk = new Aws\Sdk(['region' => 'us-east-1', 'version' => 'latest']);
